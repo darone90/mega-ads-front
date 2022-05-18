80a90143842bf0f85d44b75bd9535702a8cc24a8
Dariusz Pilecki <pileckidariusz90@gmail.com>
2022-05-18 17:30:09 +0200

    main components structure and css layout

---
+++ src/coponents/Map/Map.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './map.css';
+
+export const Map = () => {
+    return (
+        <div className="map">
+            test...
+        </div>
+    )
+}
---
+++ src/coponents/layouts/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './header.css';
+import {Button} from '../common/Btn';
+
+export const Header = () => {
+    return (
+        <header>
+            <h1>
+                <strong>Mega </strong> Ogłoszenia
+            </h1>
+            <Button text={'Dodaj ogłoszenie'}/>
+            <div className="search">
+                <input type="text"/> <Button text={'Szukaj'}/>
+            </div>
+        </header>
+    );
+};
