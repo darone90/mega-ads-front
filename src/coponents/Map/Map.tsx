cf1d1d3cfb797748016fdf7614eb547dda58fd36
Dariusz Pilecki <pileckidariusz90@gmail.com>
2022-05-18 19:04:21 +0200

    map correctly configured

--- src/coponents/Map/Map.tsx
+++ src/coponents/Map/Map.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import '../../utils/fix-map-icon';
+
 import './map.css';
+import 'leaflet/dist/leaflet.css';
+
+
 
 export const Map = () => {
     return (
         <div className="map">
-            test...
+            <MapContainer center={[50.11864, 19.97973]} zoom={25}>
+                <TileLayer
+                    url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <Marker position={[50.11864, 19.97973]}>
+                    <Popup>
+                        <h2>Domek mój</h2>
+                        <p>Tu mieszka pan Kotełek</p>
+                    </Popup>
+                </Marker>
+            </MapContainer>
         </div>
     )
 }
