593d292e1dcd1b1a0f6013a3b7f2ce38ca8de754
Dariusz Pilecki <pileckidariusz90@gmail.com>
2022-05-21 16:15:12 +0200

    show ads on map

--- src/coponents/Map/Map.tsx
+++ src/coponents/Map/Map.tsx
@@ -1,7 +1,11 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import {SearchContext} from "../../contexts/search.context";
+
+import {SingleAd} from "./SingleAd";
+
 import '../../utils/fix-map-icon';
+import {SimpleAdEntity} from "types";
 
 import './map.css';
 import 'leaflet/dist/leaflet.css';
@@ -11,25 +15,32 @@ import 'leaflet/dist/leaflet.css';
 export const Map = () => {
 
     const {search} = useContext(SearchContext)
+    const [ads, setAds] = useState<SimpleAdEntity[]>([]);
 
     useEffect(() => {
-        console.log('zapytanie do bazy za każdym razem jak zmieni się search')
+        (async () => {
+            const res = await fetch(`http://localhost:3030/ad/search/${search}`);
+            const data = await res.json();
+
+            setAds(data);
+        })();
     }, [search])
 
     return (
         <div className="map">
-            <h1>Search for: {search}</h1>
+
             <MapContainer center={[50.11864, 19.97973]} zoom={25}>
                 <TileLayer
                     url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 />
-                <Marker position={[50.11864, 19.97973]}>
-                    <Popup>
-                        <h2>Domek mój</h2>
-                        <p>Tu mieszka pan Kotełek</p>
-                    </Popup>
-                </Marker>
+                {ads.map(ad => (
+                    <Marker key={ad.id} position={[ad.lat, ad.lon]}>
+                        <Popup>
+                            <SingleAd id={ad.id}/>
+                        </Popup>
+                    </Marker>
+                ))}
             </MapContainer>
         </div>
     )
---
+++ src/coponents/Map/SingleAd.tsx
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from "react";
+import {AdTypes} from "types";
+
+
+interface Props {
+    id: string;
+}
+
+export const SingleAd = (props: Props) => {
+
+    const [ad, setAd] = useState<AdTypes | null>(null)
+
+    useEffect(() => {
+        (async () => {
+            const res = await fetch(`http://localhost:3030/ad/${props.id}`);
+            const data = await res.json();
+
+            setAd(data);
+        })()
+    })
+
+    if (!ad) return <p>Wczytywanie...</p>
+
+    return (
+        <>
+            <h2>{ad.name}</h2>
+            <p>{ad.description}</p>
+            {ad.price && <p><b>{ad.price} zł</b></p>}
+            <a href={ad.url} target="_blank">Otwórz ogłoszenie</a>
+        </>
+    )
+}
